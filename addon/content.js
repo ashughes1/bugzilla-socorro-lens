40c1232c5c842cb17e1457923f016459af6c803a
Anthony Hughes <anthony.s.hughes@gmail.com>
2016-08-19 13:52:47 -0700

    Add support for experimental UI on bugzilla.mozilla.org

--- addon/content.js
+++ addon/content.js
@@ -1,17 +1,15 @@
-var container = document.getElementById("cf_crash_signature_edit_container");
-var nodes = container.childNodes;
-var signatures = "";
-for (var i=0; i<nodes.length; i++) {
-    if (nodes[i].tagName == "A") {
-        if (nodes[i].text.indexOf("[@") !=-1) {
-            signatures = signatures + nodes[i].text.substring(3, nodes[i].text.length-2) + "\\";
-        }
-    }
-}
-if (signatures.length > 0) {
-    signatures = signatures.substring(0,signatures.length-2);
+var selector = (document.getElementById("cf_crash_signature_edit_container")) ? "cf_crash_signature_edit_container" : "field-value-cf_crash_signature";
+var querystring = "";
+document.querySelectorAll("#" + selector + " a").forEach(function(element) {
+    if (element.innerText.indexOf("[@") != -1)
+        querystring = querystring + element.innerText.substring(3, element.innerText.length-2) + "\\";        
+});
+
+if (querystring.length > 0) {
+    querystring = querystring.substring(0,querystring.length-2);
     var iframe = document.createElement('iframe');
-    iframe.src = 'https://ashughes1.github.io/bugzilla-socorro-lens/chart.htm?s=' + signatures;
+    iframe.src = 'https://ashughes1.github.io/bugzilla-socorro-lens/chart.htm?s=' + querystring;
     iframe.setAttribute('style', 'border:0;');
-    container.appendChild(iframe);
+    var container = document.getElementById(selector);
+    if (container) container.appendChild(iframe);
 }
